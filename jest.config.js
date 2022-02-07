3454743d3257ad08a2f4366e0e2697772037547e
Kleiton Barone Semolini <kleitonpolar6@gmail.com>
2022-02-07 20:40:41 -0300

    Configuração Inicial NestJS

---
+++ jest.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  moduleFileExtensions: ['ts', 'js', 'tsx', 'jsx'],
+  preset: 'ts-jest',
+  coverageReporters: ['json-summary', 'text', 'lcov'],
+  coverageThreshold: {
+    global: {
+      statements: 80,
+      branches: 80,
+      functions: 80,
+      lines: 80,
+    },
+  },
+  rootDir: 'src',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: [
+    'src/**/*.service.{js,ts}',
+    '!**/node_modules/**',
+    '!**/dist/**',
+  ],
+};
